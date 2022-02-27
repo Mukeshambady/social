2398a9e26d41f2374488ceda6983dd9966b007fa
Mukeshambady <mukeshm1989@gmail.com>
2022-02-27 20:33:11 +0530

    commit

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,73 @@
+import { useContext, useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core";
+import { useHistory } from "react-router";
+
+export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const { isFetching, dispatch } = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+
+  const regHandler =(e)=>{
+    e.preventDefault();
+    history.push("/register");
+  }
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Social</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Social.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              required
+              minLength="6"
+              className="loginInput"
+              ref={password}
+            />
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Log In"
+              )}
+            </button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton" onClick={regHandler}>
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Create a New Account"
+              )}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
